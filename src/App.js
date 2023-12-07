72a62f5a7a2c16b0838d701307ea7430c04c98be
tanapuminn <tanapumin.fo@gmail.com>
2023-12-07 11:54:01 +0700

    add to branch textAboutMe

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Tanapumin boonbutr <span>(Fogus)</span></h1>
         <img src={img} className="App-logo" alt="logo" />
     </div>
   );
